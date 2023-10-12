7e9ff21c02a3f29138988cb1e484bede71932bed
Tina y <tyousofine@gmail.com>
2023-10-11 23:17:20 -0400

    add authForm for signup/login

--- app/(auth)/login/page.js
+++ app/(auth)/login/page.js
@@ -1,11 +1,22 @@
-import React from 'react'
+'use client'
+import AuthForm from '../AuthForm'
 
 export default function Login() {
+
+    const handleSubmit = async (e, email, password) => {
+        e.preventDefault();
+
+        console.log('user login: ', email, password)
+    }
+
+
     return (
         <main>
             <h2 className="text-center">
                 Login
             </h2>
+            <AuthForm handleSubmit={handleSubmit} />
+
         </main>
     )
 }
--- app/(auth)/signup/page.js
+++ app/(auth)/signup/page.js
@@ -1,11 +1,21 @@
-import React from 'react'
+'use client'
+import AuthForm from '../AuthForm';
 
 export default function Signup() {
+
+    const handleSubmit = async (e, email, password) => {
+        e.preventDefault();
+        console.log('user signup: ', email, password)
+
+    }
+
+
     return (
         <main>
             <h2 className="text-center">
                 Sign up
             </h2>
+            <AuthForm handleSubmit={handleSubmit} />
         </main>
     )
 }

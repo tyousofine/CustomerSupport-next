240ce3aa51434b5862ca4b85a4e45ae6899fb99b
group8info3171 <group8info3174@gmail.com>
2023-10-14 18:58:14 -0400

    add db policies

--- app/(dahsboard)/tickets/TicketList.js
+++ app/(dahsboard)/tickets/TicketList.js
@@ -1,27 +1,48 @@
 
 import Link from 'next/link'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
 
 
 
 const getTickets = async () => {
-    const res = await fetch('http://localhost:3000/api/tickets', {
-        method: "GET",
-        cache: 'no-cache'
-    })
-    const json = await res.json()
 
-    if (json.error) {
-        console.log('Error fetching tickets from db: ', error.message)
+    // const res = await fetch('http://localhost:3000/api/tickets', {
+    //     method: "GET",
+    //     cache: 'no-cache',
+
+    // })
+    // const json = await res.json()
+
+    // if (json.error) {
+    //     console.log('Error fetching tickets from db: ', json.error.message)
+    // }
+
+    // return json.data;
+
+    const supabase = createServerComponentClient({ cookies })
+
+    const { data, error } = await supabase.from('Tickets')
+        .select()
+
+    if (error) {
+        console.log(error.message)
     }
 
-    return json.data;
+    // console.log("COOKIES: ", cookies)
+    // console.log("DATA: ", data)
+    // console.log("SESSION: ", session)
+
+    return data
 }
+
+
 
 export default async function TicketList() {
     const tickets = await getTickets();
     return (
-        <>  {!tickets ? (
-            <p className='text-center'>There are no open tickets.</p>
+        <>  {tickets.length === 0 ? (
+            <p className='text-center mt-16'>There are no open tickets.</p>
         ) : (
             <>
                 {tickets.map((ticket) => (
--- app/api/tickets/route.js
+++ app/api/tickets/route.js
@@ -2,6 +2,7 @@
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { NextResponse } from "next/server";
 import { cookies } from 'next/headers';
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 
 // uncomment this if errros:
 export const dynamic = 'force-dynamic'
@@ -25,12 +26,17 @@ export async function POST(req, res) {
     return NextResponse.json({ data, error })
 }
 
-export async function GET(req) {
-    const supabase = createRouteHandlerClient({ cookies });
-    const { data, error } = await supabase.from('Tickets').select();
-    return NextResponse.json({ data, error })
 
-}
+// couldnt get the cookie work on server side.
+// export async function GET(request) {
+//     const supabase = createRouteHandlerClient({ cookies });
+//     console.log('SUPABASE: ', supabase)
+//     const { data: { session } } = await supabase.auth.getSession();
+
+//     const { data, error } = await supabase.from('Tickets').select();
+//     console.log('DATA: ', data)
+//     return NextResponse.json({ data, error })
+// }
 
 
 
